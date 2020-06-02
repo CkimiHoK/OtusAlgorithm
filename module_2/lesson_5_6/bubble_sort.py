5603f922fb615e508624bb87a3ca47f362e0afaf
CkimiHoK <a.veselov.84@yandex.ru>
2020-06-03 00:07:05 +0300

    [NF] Added solves for bubble and selection sort.

--- module_2/lesson_5_6/bubble_sort.py
+++ module_2/lesson_5_6/bubble_sort.py
@@ -1,2 +1,16 @@
 def bubble_sort(str_array):
-    pass
+    """ Sort array with bubble algorithm. """
+    count = int(str_array[0])
+    collection = [int(element) for element in str(str_array[1]).split(' ')]
+
+    for i in range(count):
+        swap_exists = False
+        for j in range(count - i - 1):
+            if collection[j] > collection[j + 1]:
+                swap_exists = True
+                collection[j],  collection[j + 1] = collection[j + 1],  collection[j]
+
+        if not swap_exists:
+            break
+
+    return ' '.join(map(str, collection))
--- module_2/lesson_5_6/selection_sort.py
+++ module_2/lesson_5_6/selection_sort.py
@@ -1,2 +1,22 @@
 def selection_sort(str_array):
-    pass
+    """ Sort array with selection algorithm. """
+    count = int(str_array[0])
+    collection = [int(element) for element in str(str_array[1]).split(' ')]
+
+    for i in range(count):
+        swap_exists = False
+        index = i
+        min_element = collection[index]
+        for j in range(i + 1, count):
+            if min_element > collection[j]:
+                swap_exists = True
+                index = j
+                min_element = collection[index]
+
+        if index != i:
+            collection[i], collection[index] = collection[index], collection[i]
+
+        if not swap_exists:
+            break
+
+    return ' '.join(map(str, collection))
